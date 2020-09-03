8f78ceda0826d871ce5fcdaa1e479321e97ae478
Janice Asmbrd <jnasmbrd@gmail.com>
2020-09-03 15:59:54 -0600

    Showing the current Temperature and Update the time; Edit CSS

---
+++ src/index.js
@@ -0,0 +1,40 @@
+//timestamp is going to be the number of milliseconds that has happened since 1970
+function formatDate(timestamp) {
+  //calculate the date by 1000s see below in response.data
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  let day = days[date.getDay()];
+  return `  ${day}, ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  //console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  //times 1000, to convert to current date & time
+}
+
+let apiKey = "6df68f5433f668287bfc545331edd9d1";
+let units = "metric";
+let city = "Toronto";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
